24487bf7b4df230a73d89560b9fece02df95f9f2
niehaisheng <haishengnie@163.com>
2021-02-02 14:41:47 +0800

    首次提交，搭建好springboot架构

---
+++ mbiserver/src/main/java/com/csrcbank/mbiserver/MbiserverApplication.java
@@ -0,0 +1,13 @@
+package com.csrcbank.mbiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbiserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbiserverApplication.class, args);
+    }
+
+}
